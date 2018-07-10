e513e3977969fcdc25ba23d393185b2a991f826c
Francisco <fjgutierrez112@gmail.com>
2018-07-10 16:46:13 -0400

    scaffolded components

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p>jdkjkdfd</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
