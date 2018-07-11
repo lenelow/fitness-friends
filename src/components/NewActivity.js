8137dd69ed228481b1de54cf91463abe14b18a9f
Marissa Wood <marissa.espina@gmail.com>
2018-07-11 14:57:42 -0400

    created new activity form component and tested it locally

--- src/components/NewActivity.js
+++ src/components/NewActivity.js
@@ -1,40 +1,88 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
 class NewActivity extends Component {
-    render() {
-        constructor () {
-            super ()
-            this.state = {
-                name: '',
-                location: '',
-                date: '',
-                time: '',
-                description: ''
-            }
-        }
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      location: "",
+      date: "",
+      time: "",
+      description: "",
+      redirect: false
+    };
+  }
 
-        handleChange = () => {
-            // code this function to update the state with the inputs
-        }
+  handleChange = e => {
+    // code this function to update the state with the inputs
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
 
-        handleSubmit = () => {
-            // code this function to send the create request to the api
-        }
+    this.setState({
+      [name]: value
+    });
+  };
 
-        return (
-            <div className="new-activity">
-            <h1>New Activity: </h1>
-                <form>
-                    <input type="text" name="name" onChange={this.handleChange}>Activity Name:</input>
-                    <input type="text" name="location" onChange={this.handleChange}>Location:</input>
-                    <input type="text" name="date" onChange={this.handleChange}>Date: </input>
-                    <input type="text" name="time" onChange={this.handleChange}>Time: </input>
-                    <textarea name="description" onChange={this.handeChange}>Description:</textarea>
-                    <input type="submit" value="submit" onClick={this.handleSubmit}/>
-                </form>
-            </div>
-        );
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="/" />;
     }
+    //change this redirect later
+  };
+
+  handleSubmit = e => {
+    // code this function to send the create request to the api
+    e.preventDefault();
+    const activity = {
+      name: this.state.name,
+      location: this.state.location,
+      date: this.state.date,
+      time: this.state.time,
+      description: this.state.description
+    };
+    axios
+      .post("http://localhost:3001/api/activity/", activity)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.setState({
+          redirect: true
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="new-activity">
+        {this.renderRedirect()}
+        <h1>New Activity: </h1>
+        <form>
+          <label>Activity Name</label>
+          <input type="text" name="name" onChange={this.handleChange} />
+          <br />
+          <label>Location:</label>
+          <input type="text" name="location" onChange={this.handleChange} />
+          <br />
+          <label>Date:</label>
+          <input type="text" name="date" onChange={this.handleChange} />
+          <br />
+          <label>Time:</label>
+          <input type="text" name="time" onChange={this.handleChange} />
+          <br />
+          <label>Description:</label>
+          <textarea name="description" onChange={this.handleChange} />
+          <br />
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default NewActivity;
