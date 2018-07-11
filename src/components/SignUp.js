61b5d04b18fe0882778d53cd7a6e1cc5b103c345
Francisco <fjgutierrez112@gmail.com>
2018-07-11 11:08:46 -0400

    made edits to signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -4,35 +4,36 @@ class SignUp extends Component {
   render() {
     return 
     <div>
-    <input type="text" value="Username"/>
-    <input type="password" value="Password" />
+    <input type="text" name="username" value="Username"/>
+    <input type="password" name="password" value="Password" />
     <input type="image" alt="Signup" />
+    <textarea name="bio" Tell us about yourself and what sparks your love for fitness />
     <h3>preferences:</h3>
-    <input type="checkbox" name="biking" value="true">
+    <input type="checkbox" name="preferences[biking]" value="true">
     <label>Biking</label>
     <br />
-    <input type="checkbox" name="running" value="true">
+    <input type="checkbox" name="preferences[running]" value="true">
     <label>Running</label>
     <br />
-    <input type="checkbox" name="swimming" value="true">
+    <input type="checkbox" name="preferences[swimming]" value="true">
     <label>Swimming</label>
     <br />
-    <input type="checkbox" name="cycling" value="true">
+    <input type="checkbox" name="activitiy[cycling]" value="true">
     <label>Cycling</label>
     <br />
-    <input type="checkbox" name="yoga" value="true">
+    <input type="checkbox" name="preferences[yoga]" value="true">
     <label>Yoga</label>
     <br />
-    <input type="checkbox" name="pilates" value="true">
+    <input type="checkbox" name="preferences[pilates]" value="true">
     <label>Pilates</label>
     <br />
-    <input type="checkbox" name="weightlifting" value="true">
+    <input type="checkbox" name="preferences[weightlifting]" value="true">
     <label>weightlifting</label>
     <br />
-    <input type="checkbox" name="crossfit" value="true">
+    <input type="checkbox" name="preferences[crossfit]" value="true">
     <label>Crossfit</label>
     <br />
-    <input type="checkbox" name="solidcore" value="true">
+    <input type="checkbox" name="preferences[solidcore]" value="true">
     <label>Solidcore</label>
     </div>
   }
