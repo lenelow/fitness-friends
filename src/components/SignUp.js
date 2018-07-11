cd7422b4db08a00b5f330513ed7f51318e15dc7a
Francisco <fjgutierrez112@gmail.com>
2018-07-11 10:41:51 -0400

    built out signup component

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,9 +2,44 @@ import React, { Component } from "react";
 
 class SignUp extends Component {
   render() {
-    return;
-    <div />;
+    return 
+    <div>
+    <input type="text" value="Username"/>
+    <input type="password" value="Password" />
+    <input type="image" alt="Signup" />
+    <h3>preferences:</h3>
+    <input type="checkbox" name="biking" value="true">
+    <label>Biking</label>
+    <br />
+    <input type="checkbox" name="running" value="true">
+    <label>Running</label>
+    <br />
+    <input type="checkbox" name="swimming" value="true">
+    <label>Swimming</label>
+    <br />
+    <input type="checkbox" name="cycling" value="true">
+    <label>Cycling</label>
+    <br />
+    <input type="checkbox" name="yoga" value="true">
+    <label>Yoga</label>
+    <br />
+    <input type="checkbox" name="pilates" value="true">
+    <label>Pilates</label>
+    <br />
+    <input type="checkbox" name="weightlifting" value="true">
+    <label>weightlifting</label>
+    <br />
+    <input type="checkbox" name="crossfit" value="true">
+    <label>Crossfit</label>
+    <br />
+    <input type="checkbox" name="solidcore" value="true">
+    <label>Solidcore</label>
+    </div>
   }
 }
 
 export default SignUp;
+
+
+User: username, email, password, profile picture
+Profile: username, profile picture, preferences/interest, friendlist, schedule 
