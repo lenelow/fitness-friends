a05335610e4c192e3970ff8dac8cc6a08097cfb5
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-12 19:43:02 -0400

    starting on routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import './App.css'
-// import SignUp from "./SignUp";
+import Welcome from './Welcome'
 import Profile from './Profile'
+import Login from './Login'
+import { Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Fitness Friends</h1>
-        </header>
-        <Profile />
+        <nav className='nav'>
+          <Link to='/'><h1 className='logo'>Fitness Friends</h1></Link>
+          <Link to='/profile/:id'><h1 className='navButton myProfile'>My Profile</h1></Link>
+          <Link to='/login'><h1 className='navButton'>Logout</h1></Link>
+        </nav>
+        <main>
+          <Route
+            path='/'
+            exact
+            component={Welcome}
+          />
+          <Route
+            path='/profile/:id'
+            exact
+            component={Profile}
+          />
+          <Route
+            path='/login'
+            exact
+            component={Login}
+          />
+        </main>
       </div>
     )
   }
