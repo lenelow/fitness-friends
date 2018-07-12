9e6e432edc429376c5cd8325e6a1d74a7ef8b0db
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-12 09:07:50 -0400

    adding bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 import React, { Component } from 'react'
 import './App.css'
-import Profile from './Profile'
-=======
-import React, { Component } from "react";
-import "./App.css";
-import Login from "./Login";
 // import SignUp from "./SignUp";
-import Welcome from "./Welcome";
->>>>>>> 233af6f4fc76bbc3428acd24794266a74cfac742
+import Login from './Login'
 
 class App extends Component {
   render () {
@@ -17,15 +10,7 @@ class App extends Component {
         <header className='App-header'>
           <h1 className='App-title'>Fitness Friends</h1>
         </header>
-        <a href='/signup' id='signup'>
-          Don't have an account? Sign up here!
-        </a>
-<<<<<<< HEAD
-        <Profile />
-=======
-        {/* <SignUp /> */}
-        <Welcome />
->>>>>>> 233af6f4fc76bbc3428acd24794266a74cfac742
+        <Login />
       </div>
     )
   }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Profile.css'
 
 class Profile extends Component {
   constructor (props) {
@@ -8,18 +9,19 @@ class Profile extends Component {
       user: 'Jimmy Byess',
       image: 'http://img2.timeinc.net/people/i/2015/cbb/blog/150518/jimmy-fallon-02-300x400.jpg',
       bio: 'I love to make bread and go running',
-      preferences: ['running', 'biking', 'weightlifting']
+      preferences: ['running', ' biking', 'weightlifting']
     }
   }
   render () {
     return (
       <div>
-        <h1>{this.state.user}'s Profile</h1>
-        <img src={this.state.image} />
-        <p>{this.state.bio}</p>
+        <h1 className='usersName'>{this.state.user}'s Profile</h1>
+        <img src={this.state.image} height='200' width='150' />
+        <p><text>About Me:</text> {this.state.bio}</p>
         <ul>
-          <li>{this.state.preferences[0]}</li>
-          <li>{this.state.preferences[1]}</li>
+          <text className='title'>My Interests: </text>
+          <li>{this.state.preferences[0]},</li>
+          <li>{this.state.preferences[1]},</li>
           <li>{this.state.preferences[2]}</li>
         </ul>
       </div>
