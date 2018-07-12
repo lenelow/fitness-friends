7aa107b5e6c9dde768b503a49e6578d5f0b555e7
Francisco <fjgutierrez112@gmail.com>
2018-07-12 15:07:25 -0400

    added handleInput method in app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from "react";
 import "./App.css";
-// import SignUp from "./SignUp";
+import SignUp from "./SignUp";
 import Profile from "./Profile";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+      isLoggedIn: false
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
