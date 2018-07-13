a13411eca7aeced300c0479d67434f088baa728a
Leah Enelow <lenelow@students.terpmail.edu>
2018-07-13 10:27:59 -0400

    initial reformatting

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import data from './profile-data.json'
-import './FriendList.css'
+import React, { Component } from "react";
+import data from "./profile-data.json";
+import "./FriendList.css";
 
 class FriendsList extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <h1 className='myFriends'>My Friends</h1>
-        <div className='friendList'>
+        <h1 className="myFriends">My Friends</h1>
+        <div className="friendList">
           <ul>
             {data.map(user => (
               <li>
-                <div className='friend'>
-                  <img src={user.image} height='150' width='150' />
+                <div className="friend">
+                  <img src={user.image} height="150" width="150" />
                   <h3>{user.username}</h3>
                 </div>
               </li>
@@ -20,8 +20,8 @@ class FriendsList extends Component {
           </ul>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default FriendsList
+export default FriendsList;
